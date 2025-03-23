9c4e32a0e2fd1a12ab4ee4471c76cc4b5e0fe976
yuriyzhuk <zhukyuri@gmail.com>
2025-03-23 21:10:37 +0200

    Rafactoring cashe user name

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React from 'react';
-import Header from './Header';
-import Banner from './Banner';
-import Timer from './Timer';
-import Gifts from './Gifts';
-import EventDetails from './EventDetails';
-import Schedule from './Schedule';
-import Playlist from './Playlist';
-import DressCode from './DressCode';
-import RSVPFormDemo from './RSVPFormDemo';
-import './App.css';
+import React from 'react'
+import Header from './Header'
+import Banner from './Banner'
+import Timer from './Timer'
+import Gifts from './Gifts'
+import EventDetails from './EventDetails'
+import Schedule from './Schedule'
+import Playlist from './Playlist'
+import DressCode from './DressCode'
+import RSVPFormDemo from './RSVPFormDemo'
+import './App.css'
 
 function App() {
+  const queryString = new URLSearchParams(window.location.search)
+  let value = queryString.get('USERID')
+  const keyName = 'userId'
+  if (value) {
+    sessionStorage.setItem(keyName, value)
+    localStorage.setItem(keyName, value)
+  }
+
+  if (!value) {
+    value = localStorage.getItem(keyName)
+    if (value) sessionStorage.setItem(keyName, value)
+    if (!value) sessionStorage.setItem(keyName, 'Гість')
+  }
+
   return (
     <div className="App">
       <Header />
       <Banner />
-      <Timer /> 
+      <Timer />
       <Gifts />
       <EventDetails />
       <Schedule />
       <DressCode />
-      <Playlist/>
+      <Playlist />
       <RSVPFormDemo />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
